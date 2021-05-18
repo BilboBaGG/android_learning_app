3dffc44bbd9e213d58983b950950ce941dcddc84
antidimon <ivasenkodiman@mail.ru>
2021-05-18 16:23:17 +0300

    added file info.txt, added writing in file

--- testforbeta.py
+++ testforbeta.py
@@ -18,6 +18,8 @@ class MainApp(App):
 			self.massiv2[i].text = "0"
 			if a == self.arr[i]:
 				self.massiv2[i].text = "1"
+				file = open('info.txt', 'w')
+				file.write(self.arr[i]) 
 
 	def build(self):
 
