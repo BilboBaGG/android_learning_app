bc8489193f89487e37132bcfd5cd5034d7ee299d
antidimon <ivasenkodiman@mail.ru>
2021-06-18 13:50:03 +0300

    test

--- test.py
+++ test.py
@@ -6,116 +6,64 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.core.window import Window
 
+CONST = ["География","Английский язык","Математика","Информатика","Русский язык","Физика","История","Музыка"]
+
 Window.clearcolor = (1, .97 , .86 , 1)
 
 class MainApp(App):
 
-	def making_mainscreen(self,instance):
-
-		self.vse.clear_widgets()
-		self.gl_layout.clear_widgets()
-		self.gl_layout1.clear_widgets()
-		self.gl_layout2.clear_widgets()
-		self.bl_layout.clear_widgets()
-	
-		self.gl_layout1.add_widget (Button (text = "География", background_color = (.91, .77, .5, 1), background_normal = '', color = (.13, .07, .01, 1), on_release = self.making_stranica))
-		self.gl_layout1.add_widget (Button (text = "Английский язык", background_color = (.91, .77, .5, 1), background_normal = '', color = (.13, .07, .01, 1), on_release = self.making_stranica))
-		self.gl_layout1.add_widget (Button (text = "Математика", background_color = (.91, .77, .5, 1), background_normal = '', color = (.13, .07, .01, 1), on_release = self.making_stranica))
-		self.gl_layout1.add_widget (Button (text = "Информатика", background_color = (.91, .77, .5, 1), background_normal = '', color = (.13, .07, .01, 1), on_release = self.making_stranica))
-		self.gl_layout1.add_widget (Button (text = "Русский язык", background_color = (.91, .77, .5, 1), background_normal = '', color = (.13, .07, .01, 1), on_release = self.making_stranica))
-		self.gl_layout1.add_widget (Button (text = "Физика", background_color = (.91, .77, .5, 1), background_normal = '', color = (.13, .07, .01, 1), on_release = self.making_stranica))
-		self.gl_layout1.add_widget (Button (text = "*", background_color = (.91, .77, .5, 1), background_normal = '', color = (.13, .07, .01, 1), on_release = self.making_stranica))
-		self.gl_layout1.add_widget (Button (text = "№", background_color = (.91, .77, .5, 1), background_normal = '', color = (.13, .07, .01, 1), on_release = self.making_stranica))
-		
-		self.gl_layout2.add_widget(Label(text = "0", color = (.13, .07, .01, 1)))
-		self.gl_layout2.add_widget(Label(text = "0", color = (.13, .07, .01, 1)))
-		self.gl_layout2.add_widget(Label(text = "0", color = (.13, .07, .01, 1)))
-		self.gl_layout2.add_widget(Label(text = "0", color = (.13, .07, .01, 1)))
-		self.gl_layout2.add_widget(Label(text = "0", color = (.13, .07, .01, 1)))
-		self.gl_layout2.add_widget(Label(text = "0", color = (.13, .07, .01, 1)))
-		self.gl_layout2.add_widget(Label(text = "0", color = (.13, .07, .01, 1)))
-		self.gl_layout2.add_widget(Label(text = "0", color = (.13, .07, .01, 1)))
-
-		self.gl_layout.add_widget(self.gl_layout1)
-		self.gl_layout.add_widget(self.gl_layout2)
-		
-		self.lb = Label (text = "ADRON", color = (.13, .07, .01, 1), font_size = 30, size_hint = (1, .07))
-
-		self.bl_layout.add_widget (Button (text = "left", background_color = (.91, .77, .5, 1), background_normal = '', color = (.13, .07, .01, 1)))
-		self.bl_layout.add_widget (Button (text= "right", background_color = (.91, .77, .5, 1), background_normal = '', color = (.13, .07, .01, 1)))
-
-		self.vse.add_widget(self.lb)
-		self.vse.add_widget(self.gl_layout)
-		self.vse.add_widget(self.bl_layout)
-
-	def making_stranica(self, instance):
+	def vibor(self, instance):
 		
 		a = str(instance.text)
-		for self.i in range(len(self.arr)): 
-			if a == self.arr[self.i]:
-				self.stranica = self.i
 		
-		self.vse.clear_widgets()
-		self.bl_layout.clear_widgets()
-		
+		for i in range(8):
+			self.massiv2[i].text = "0"
 
-		self.gl_layout3 = GridLayout (cols = 1,spacing = 5, size_hint = (1, .75))
-
-		self.gl_layout3.add_widget(Button(text = "1", background_color = (.91, .77, .5, 1), background_normal = '', color = (.13, .07, .01, 1)))
-		self.gl_layout3.add_widget(Button(text = "2", background_color = (.91, .77, .5, 1), background_normal = '', color = (.13, .07, .01, 1)))
-		self.gl_layout3.add_widget(Button(text = "3", background_color = (.91, .77, .5, 1), background_normal = '', color = (.13, .07, .01, 1)))
-		self.gl_layout3.add_widget(Button(text = "4", background_color = (.91, .77, .5, 1), background_normal = '', color = (.13, .07, .01, 1)))
-		self.gl_layout3.add_widget(Button(text = "5", background_color = (.91, .77, .5, 1), background_normal = '', color = (.13, .07, .01, 1)))
-		self.gl_layout3.add_widget(Button(text = "6", background_color = (.91, .77, .5, 1), background_normal = '', color = (.13, .07, .01, 1)))
-		self.gl_layout3.add_widget(Button(text = "7", background_color = (.91, .77, .5, 1), background_normal = '', color = (.13, .07, .01, 1)))
-		self.gl_layout3.add_widget(Button(text = "8", background_color = (.91, .77, .5, 1), background_normal = '', color = (.13, .07, .01, 1)))
-
-		self.bl_layout.add_widget(Button(text ="Главный экран", background_color = (.91, .77, .5, 1), background_normal = '', color = (.13, .07, .01, 1), on_release = self.making_mainscreen))
-
-		self.lb = Label(text = self.arr[self.stranica], color = (.13, .07, .01, 1), font_size = 30, size_hint = (1, .07))
-
-		self.vse.add_widget(self.lb)
-		self.vse.add_widget(self.gl_layout3)
-		self.vse.add_widget(self.bl_layout)
+			if a == self.arr[i]:
+				self.massiv2[i].text = "1"
+				file = open('var.txt', 'w')
+				file.write(str(i))
+				file.close() 
 
 	def build(self):
 
-		self.arr = ["Главный экран","География","Английский язык","Математика","Информатика","Русский язык","Физика","*","№"]
+		self.arr = CONST
 		self.i = 0
-			
 		self.gl_layout = GridLayout (cols = 2,spacing = 5, size_hint = (1, .75))
 
-		self.gl_layout1 = GridLayout(cols = 1, spacing = 5, size_hint = (.92, 1))
-		
-		self.gl_layout1.add_widget (Button (text = "География", background_color = (.91, .77, .5, 1), background_normal = '' , color = (.13, .07, .01, 1), on_release = self.making_stranica))
-		self.gl_layout1.add_widget (Button (text = "Английский язык", background_color = (.91, .77, .5, 1), background_normal = '', color = (.13, .07, .01, 1),on_release = self.making_stranica))
-		self.gl_layout1.add_widget (Button (text = "Математика", background_color = (.91, .77, .5, 1), background_normal = '', color = (.13, .07, .01, 1),on_release = self.making_stranica))
-		self.gl_layout1.add_widget (Button (text = "Информатика", background_color = (.91, .77, .5, 1), background_normal = '', color = (.13, .07, .01, 1),on_release = self.making_stranica))
-		self.gl_layout1.add_widget (Button (text = "Русский язык", background_color = (.91, .77, .5, 1), background_normal = '', color = (.13, .07, .01, 1),on_release = self.making_stranica))
-		self.gl_layout1.add_widget (Button (text = "Физика", background_color = (.91, .77, .5, 1), background_normal = '', color = (.13, .07, .01, 1),on_release = self.making_stranica))
-		self.gl_layout1.add_widget (Button (text = "*", background_color = (.91, .77, .5, 1), background_normal = '', color = (.13, .07, .01, 1),on_release = self.making_stranica))
-		self.gl_layout1.add_widget (Button (text = "№", background_color = (.91, .77, .5, 1), background_normal = '', color = (.13, .07, .01, 1),on_release = self.making_stranica))
+		#grid with theme buttons
+		self.gl_layout1 = GridLayout(cols = 1, spacing = 16, size_hint = (.82, 1))
+		self.massiv = [Button(text = self.arr[i], background_color = (.91, .77, .5, 1), background_normal = '' , color = (.13, .07, .01, 1), on_release = self.vibor) for i in range(8)]
 
-		self.gl_layout2 = GridLayout (cols = 1, spacing = 5, size_hint = (.05, 1))
-		
-		self.gl_layout2.add_widget(Label(text = "0", color = (.13, .07, .01, 1)))
-		self.gl_layout2.add_widget(Label(text = "0", color = (.13, .07, .01, 1)))
-		self.gl_layout2.add_widget(Label(text = "0", color = (.13, .07, .01, 1)))
-		self.gl_layout2.add_widget(Label(text = "0", color = (.13, .07, .01, 1)))
-		self.gl_layout2.add_widget(Label(text = "0", color = (.13, .07, .01, 1)))
-		self.gl_layout2.add_widget(Label(text = "0", color = (.13, .07, .01, 1)))
-		self.gl_layout2.add_widget(Label(text = "0", color = (.13, .07, .01, 1)))
-		self.gl_layout2.add_widget(Label(text = "0", color = (.13, .07, .01, 1)))
+
+		for i in range(8):
+			self.gl_layout1.add_widget(self.massiv[i])
+
+
+		#grid with 0 1
+		self.gl_layout2 = GridLayout (cols = 1, spacing = 16, size_hint = (.15, 1))	
+		self.massiv2 = [Label(text = "0", font_size = 40, color = (.13, .07, .01, 1)) for _ in range(8)]
+
+		file = open('var.txt', 'r')
+		self.massiv2[int(file.read())].text = "1"
+		file.close()
+
+
+		for i in range(8):
+			self.gl_layout2.add_widget(self.massiv2[i])
+
 
 		self.gl_layout.add_widget(self.gl_layout1)
 		self.gl_layout.add_widget(self.gl_layout2)
 		
-		self.lb = Label (text = "ADRON", color = (.13, .07, .01, 1), font_size = 30, size_hint = (1, .07))
+		self.lb = Label (text = "ADRON", color = (.13, .07, .01, 1), font_size = 50, size_hint = (1, .1))
 
-		self.bl_layout = BoxLayout (size_hint = (1, .1), spacing = 5)
-		self.bl_layout.add_widget (Button (text = "left", background_color = (.91, .77, .5, 1), background_normal = '', color = (.13, .07, .01, 1)))
-		self.bl_layout.add_widget (Button (text= "right", background_color = (.91, .77, .5, 1), background_normal = '', color = (.13, .07, .01, 1)))
+		#boxlayout for 2 buttons
+		self.bl_layout = BoxLayout (size_hint = (1, .07), spacing = 16)
+		self.bl_layout.add_widget (Button (text = "назад", background_color = (.91, .77, .5, 1), background_normal = '', color = (.13, .07, .01, 1)))
+		self.bl_layout.add_widget (Button (text= "вперёд", background_color = (.91, .77, .5, 1), background_normal = '', color = (.13, .07, .01, 1)))
 
+		#all in one window
 		self.vse = BoxLayout(orientation = "vertical", padding = 20,spacing = 10)
 		self.vse.add_widget(self.lb)
 		self.vse.add_widget(self.gl_layout)
